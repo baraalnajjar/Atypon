77ceeb068aff6a33cfcc1bb78d2d7922ffaec2e6
baraa <baraanajjarc@gmail.com>
2024-06-22 03:03:27 +0300

    div feature

--- SimpleCalculater/src/main/java/org/example/Main.java
+++ SimpleCalculater/src/main/java/org/example/Main.java
@@ -6,12 +6,17 @@ public class Main {
         System.out.print("addition of a and b = ");  System.out.println(a = b);
         return a = b;
     }
+    public static double div(double a, double b) {
+        System.out.print("division of a and b = ");  System.out.println(a / b);
+        return a / b;
+    }
     public static void main(String[] args)
     {
         int a=6;
         int b=3;
         add( a,  b);
-
+        div( a,  b);
+        sub( a,  b);
 
     }
 }
