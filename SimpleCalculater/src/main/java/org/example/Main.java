04ae70049e449d3a965e8b34c8846860661c8e07
baraa <baraanajjarc@gmail.com>
2024-06-22 03:00:25 +0300

    mul feature

--- SimpleCalculater/src/main/java/org/example/Main.java
+++ SimpleCalculater/src/main/java/org/example/Main.java
@@ -6,12 +6,16 @@ public class Main {
         System.out.print("addition of a and b = ");  System.out.println(a = b);
         return a = b;
     }
+    public static double mul(double a, double b) {
+        System.out.print("multiplication of a and b = ");  System.out.println(a * b);
+        return a * b;
+    }
+
     public static void main(String[] args)
     {
         int a=6;
         int b=3;
         add( a,  b);
-
-
+        mul( a,  b);
     }
 }
